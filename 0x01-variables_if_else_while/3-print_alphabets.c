82197bea6eab510bd84cc4308289b94c65aa36b6
Gershom-pro <giftjoel.gashom@gmail.com>
2022-06-03 20:44:30 -0700

    2 to 5

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *  main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ *  Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+char alphabets;
+
+for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+
+putchar(alphabets);
+
+
+for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+putchar(alphabets);
+
+putchar('\n');
+
+
+return (0);
+
+
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - prints all single digit numbers of base 10 starting from 0.
+ *
+ *  Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+
+printf("%d\n", digit);
+
+return (0);
+
+
+}
