187d001ac9faa3adc99af8be23f34bd22f9881b2
Gershom-pro <giftjoel.gashom@gmail.com>
2022-06-10 10:27:36 -0700

    3-print_numbers.c updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  *
- * Return: void.
+ * Return: Numbers 0 - 9.
  */
 
 void print_numbers(void)
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		_putchar(a);
+		_putchar(a + '0');
 	}
 
 	_putchar('\n');
