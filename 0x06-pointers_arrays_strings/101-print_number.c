8b863c0606a5187386d8a9d88fed647d5ffa1a8e
Gershom-pro <giftjoel.gashom@gmail.com>
2022-06-16 21:59:41 -0700

    101-print_number.c updated

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,20 +7,30 @@
 
 void print_number(int n)
 {
-	unsigned int nu;
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
 
-	if (n < 0)
-	{
-		nu = -n;
-		_putchar('-');
-	}
+	if (n == 0)
+		_putchar('0');
 	else
 	{
-		nu = n;
+		if (n < 0)
+		{
+
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (t_beg <= positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
-
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
 }
