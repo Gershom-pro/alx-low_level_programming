fd314b1ad953d8b003c83031a940093fd782403f
Gershom-pro <giftjoel.gashom@gmail.com>
2022-06-18 17:06:07 -0700

    103-infinite_add.c updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,75 +1,71 @@
 #include "main.h"
 
 /**
- * a_to_int - get integer value of string of characters
- * @s: string to change to integer
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
  *
- * Return: integer value of string
- */
-
-int a_to_int(char *s)
-{
-	int i = 0;
-	int size = 0;
-	int place = 1;
-	int num = 0;
-
-	while (s[i])
-	{
-		size++;
-		i++;
-	}
-	for (i = (size - 1); i >= 0; i--)
-	{
-		num += ((s[i] - '0') * place);
-		place *= 10;
-	}
-	return (num);
-
-}
-
-
-/**
- * infinite_add - adds two numbers.
- * @n1: first input number
- * @n2: second input number
- * @r: input buffer to return
- * @size_r: size of buffer
- *
- * Return: pointer to buffer or 0 if size of result is
- * greater than buffer size
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int num1, num2, sum, n, digits, i;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	num1 = a_to_int(n1);
-	num2 = a_to_int(n2);
-	sum = num1 + num2;
-	n = sum;
-	digits = 1;
-	i = 0;
-
-	while ((n / 10) != 0)
-	{
-		digits++;
-		n /= 10;
-	}
-
-	if ((digits + 1) > size_r)
-	{
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
 		return (0);
-	}
-	else
-	{
-		r[digits] = '\0';
-		for (i = (digits - 1); i >= 0; i--)
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2){
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
 		{
-			r[i] = (sum % 10) + '0';
-			sum /= 10;
-		}
-	}
+			carry = 1;
+			sum -= 10;
 
+		}
+		else
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
+
+	}
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
+		i++;
+
+	}
+	i = 0;
+	while (i < tmpl)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
+	}
 	return (r);
 }
