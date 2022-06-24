d4c53b3011d4c448a5c523f6788681fb69115c04
Gershom-pro <giftjoel.gashom@gmail.com>
2022-06-23 19:33:05 -0700

    101-wildcmp.c updated

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,30 @@
 #include "main.h"
 
-int bandersnatch(char *s1, char *s2);
-char *move(char *s2);
+
+/**
+ * substring_match - check if a substring after wildcard matches s1
+ * @s1: one string
+ * @s2: one string
+ * @after_wldcd: placeholder for position right after wildcard
+ *
+ * Return: 1 if matched, 0 if not
+ */
+
+int substring_match(char *s1, char *s2, char *after_wldcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+	else
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+
 
 /**
  * wildcmp - compares two strings recursively,
@@ -15,65 +38,13 @@ char *move(char *s2);
 
 int wildcmp(char *s1, char *s2)
 {
-	int sum = 0;
-
-	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-
 	if (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-			return (1);
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
-
-	if (*s1 == '\0' || *s2 == '\0')
-		return (0);
-
-	if (*s2 == '*')
-	{
-		s2 = move(s2);
-		if (*s2 == '\0')
-			return (1);
-		if (*s1 == *s2)
-			sum += wildcmp(s1 + 1, s2 + 1);
-		sum += bandersnatch(s1 + 1, s2);
-		return (!!sum);
-	}
-	return (0);
-
-}
-
-/**
- * bandersnatch - checks recursively for all the paths when the
- * characters are equal
- * @s1: first string
- * @s2: second string
- *
- * Return: return value of wildcmp() or of itself
- */
-
-int bandersnatch(char *s1, char *s2)
-{
-	if (*s1 == '\0')
-		return (0);
-	if (*s1 == *s2)
-		return (wildcmp(s1, s2));
-	return (bandersnatch(s1 + 1, s2));
-}
-
-/**
- * *move - moves the current char past the *
- * @s2: string to iterate over
- *
- * Return: the address of the character after the *
- */
-
-char *move(char *s2)
-{
-	if (*s2 == '*')
-		return (move(s2 + 1));
+	else if (*s2 == '*')
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
 	else
-		return (s2);
+		return (0);
 
 }
